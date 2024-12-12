9b56ae846693d143b9b9eaf539538a8607b898b3
seduhinvova@gmail.com <seduhinvova@gmail.com>
2024-12-12 18:58:33 +0500

    first commit

---
+++ src/FractionInterface.java
@@ -0,0 +1,5 @@
+interface Fraction {
+    double getDecimalValue();  // Получение вещественного значения дроби
+    void setNumerator(int numerator);  // Установка числителя
+    void setDenominator(int denominator);  // Установка знаменателя
+}
